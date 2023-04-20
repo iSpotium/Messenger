27c23b5f93b19863384ebe04f8cead9109371bb1
iSpotium <Klililison@gmail.com>
2023-04-20 15:41:07 +0300

    First commit

---
+++ src/test/java/by/it/academy/task/Messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.it.academy.task.Messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
